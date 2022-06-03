044c496564d81dbadd8c916b5e4aa63572893384
Charles Badibanga <charlesbadibanga2000@yahoo.fr>
2022-06-03 09:59:29 -0400

    Making an ajax request to retrieve weather data

---
+++ script.js
@@ -0,0 +1,30 @@
+let ville = "Atlanta";
+
+const url =
+  "https://api.openweathermap.org/data/2.5/weather?q=" +
+  ville +
+  "&appid=8b5e0537b2b17abc10e53b7116cb33e1&units=metric";
+
+// const url = "https://blockchain.info/ticker";
+
+//update the weather automatically
+function retrievePrice() {
+  // I create a request with JS.
+  let request = new XMLHttpRequest(); // Create an object
+  request.open("GET", url); // open a new request that will use url to access API
+  request.responseType = "json"; // I want the date to be in Json format
+  request.send(); // I send my request
+
+  //Once I receive a response, this function must be executed.
+  request.onload = function () {
+    if (request.readyState === XMLHttpRequest.DONE) {
+      if (request.status === 200) {
+        let storeResponse = request.response; // Store a response
+        console.log(storeResponse);
+        document.querySelector("#price_label").textContent = DollarResponse; // To output US dollar data on the webpage
+      } else {
+        alert("An error occured! Come back later!");
+      }
+    }
+  };
+}
