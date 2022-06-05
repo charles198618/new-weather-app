6fe412151849dec8fdbc224627f106efda2cd2cf
Charles Badibanga <charlesbadibanga2000@yahoo.fr>
2022-06-05 09:13:06 -0400

    Adding event listener to choose the weather of specific city

--- script.js
+++ script.js
@@ -1,25 +1,35 @@
 let city = "Atlanta";
- 
-const url = 
-"https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=8b5e0537b2b17abc10e53b7116cb33e1&units=imperial";
 
-let request = new XMLHttpRequest(); // Create an object
-request.open("GET", url); // open a new request that will use url to access API
-request.responseType = "json"; // I want the date to be in Json format
-request.send(); // I send my request
-request.onload = function () {
-      if (request.readyState === XMLHttpRequest.DONE) {
-        if (request.status === 200) {
-          let weatherDatas = request.response; // Store a response
-          //main.temp
-          // console.log(storeResponse); 
-          let temperature = weatherDatas.main.temp; 
-          let city = this.response.name;
-          document.querySelector("#temperature_label").textContent = temperature; //output the temperature of the city on webpage
-          document.querySelector("#city").textContent = city; //output the name of the city on webpage
-        }
-        else{
-          alert("An error occured! Come back later!");
-        }
+let changeCity = document.querySelector("#change"); //How to change the city
+changeCity.addEventListener("click", () => {
+  city = prompt("what city");
+  receiveCityTemp(city);
+});
+
+receiveCityTemp = function(city) {
+  const url =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    city +
+    "&appid=8b5e0537b2b17abc10e53b7116cb33e1&units=imperial";
+
+  let request = new XMLHttpRequest(); // Create an object
+  request.open("GET", url); // open a new request that will use url to access API
+  request.responseType = "json"; // I want the date to be in Json format
+  request.send(); // I send my request
+
+  request.onload = function () {
+    if (request.readyState === XMLHttpRequest.DONE) {
+      if (request.status === 200) {
+        let weatherDatas = request.response; // Store a response
+        //main.temp
+        // console.log(storeResponse);
+        let temperature = weatherDatas.main.temp;
+        let city = this.response.name;
+        document.querySelector("#temperature_label").textContent = temperature; //output the temperature of the city on webpage
+        document.querySelector("#city").textContent = city; //output the name of the city on webpage
+      } else {
+        alert("An error occured! Come back later!");
       }
     }
+  }
+}
